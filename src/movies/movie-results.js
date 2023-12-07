dc26c7f4206ecab8a96c2015648e7905b1df3bf0
Michael Huang <michael.s.huang.01@gmail.com>
2023-12-07 16:06:04 -0500

    retry button on no search results for movies

--- src/movies/movie-results.js
+++ src/movies/movie-results.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import * as client from './movie-service';
 import { Link, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function MovieResults() {
     const [searchResults, setSearchResults] = useState(null);
+    const navigate = useNavigate();
 
     const search = async (text) => {
         const results = await client.fullTextSearch(text);
@@ -41,7 +43,12 @@ function MovieResults() {
                         </div>
                     </Link>
                 ))}
-            {searchResults && searchResults.Response == 'False' && <div> Error</div>}
+            {searchResults && searchResults.Response == 'False' && (
+                <div>
+                    <div>Error: No Result Found</div>
+                    <button onClick={() => navigate('/TissueBoxd/search/')}>Try Again</button>
+                </div>
+            )}
         </div>
     );
 }
