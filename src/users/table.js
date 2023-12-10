728dd9c753ae1eed75b6990ebc3b3889711f5864
akuatbekov23 <akuatbekov23@gmail.com>
2023-12-09 21:19:30 -0500

    table cool now

--- src/users/table.js
+++ src/users/table.js
@@ -5,10 +5,13 @@ import { Link } from 'react-router-dom';
 function UserTable() {
     const [users, setUsers] = useState([]);
     const [user, setUser] = useState({ username: '', password: '', role: 'USER' });
+    const [showPassword, setShowPassword] = useState(false);
     const createUser = async () => {
         try {
             const newUser = await client.createUser(user);
             setUsers([newUser, ...users]);
+            setUser({ username: '', password: '', role: 'USER' });
+            setShowPassword(false);
         } catch (err) {
             console.log(err);
         }
@@ -18,6 +21,7 @@ function UserTable() {
         try {
             const u = await client.findUserById(user._id);
             setUser(u);
+            setShowPassword(false);
         } catch (err) {
             console.log(err);
         }
@@ -25,8 +29,9 @@ function UserTable() {
 
     const updateUser = async () => {
         try {
-            const status = await client.updateUser(user);
-            setUsers(users.map((u) => (u._id === user._id ? user : u)));
+            await client.updateUser(user);
+            fetchUsers();
+            setShowPassword(false);            
         } catch (err) {
             console.log(err);
         }
@@ -40,7 +45,7 @@ function UserTable() {
     const deleteUser = async (user) => {
         try {
             await client.deleteUser(user);
-            setUsers(users.filter((u) => u._id !== user._id));
+            fetchUsers();
         } catch (err) {
             console.log(err);
         }
@@ -50,64 +55,132 @@ function UserTable() {
         fetchUsers();
     }, []);
     return (
-        <div>
-            <h1>User List</h1>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Password</th>
-                        <th>Username</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Role</th>
-                    </tr>
-                    <tr>
-                        <td>
+        <div className="row">
+                <div className="col-md-4">
+                    <h2>Edit User Details</h2>
+                    <form>
+                       
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">
+                                Username
+                            </label>
                             <input
-                                value={user.password}
-                                onChange={(e) => setUser({ ...user, password: e.target.value })}
-                            />
-                        </td>
-                        <td>
-                            <input
+                                type="text"
+                                className="form-control"
+                                id="username"
                                 value={user.username}
                                 onChange={(e) => setUser({ ...user, username: e.target.value })}
                             />
-                        </td>
-                        <td>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="firstName" className="form-label">
+                                First Name
+                            </label>
                             <input
+                                type="text"
+                                className="form-control"
+                                id="firstName"
                                 value={user.firstName}
                                 onChange={(e) => setUser({ ...user, firstName: e.target.value })}
                             />
-                        </td>
-                        <td>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="lastName" className="form-label">
+                                Last Name
+                            </label>
                             <input
+                                type="text"
+                                className="form-control"
+                                id="lastName"
                                 value={user.lastName}
                                 onChange={(e) => setUser({ ...user, lastName: e.target.value })}
                             />
-                        </td>
-                        <td>
-                            <select value={user.role} onChange={(e) => setUser({ ...user, role: e.target.value })}>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="role" className="form-label">
+                                Role
+                            </label>
+                            <select
+                                className="form-select"
+                                id="role"
+                                value={user.role}
+                                onChange={(e) => setUser({ ...user, role: e.target.value })}
+                            >
                                 <option value="USER">User</option>
                                 <option value="ADMIN">Admin</option>
                                 <option value="FACULTY">Faculty</option>
                                 <option value="STUDENT">Student</option>
                             </select>
-                        </td>
-                        <td>
-                            <BsPlusCircleFill onClick={createUser} />
-                            <BsFillCheckCircleFill onClick={updateUser} className="me-2 text-success fs-1 text" />
-                        </td>
+
+                        </div>
+
+                        
+                    {showPassword ? (
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">
+                                Password
+                            </label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="password"
+                                value={user.password}
+                                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                            />
+                        </div>
+                    ) : (
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                value={user.password}
+                                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                            />
+                        </div>
+                    )}
+                    <div className="mb-3 form-check">
+                        <input
+                            type="checkbox"
+                            className="form-check-input"
+                            id="showPassword"
+                            checked={showPassword}
+                            onChange={() => setShowPassword(!showPassword)}
+                        />
+                        <label className="form-check-label" htmlFor="showPassword">
+                            Show Password
+                        </label>
+                    </div>
+
+                        <button type="button" className="btn btn-primary" onClick={updateUser}>
+                            <BsFillCheckCircleFill /> Update User
+                        </button>
+                    </form>
+                </div>
+                <div className="col-md-8">
+                    <h2>User List</h2>
+                    <table className="table">
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Role</th>
+                    </tr>
+                    <tr>
+                                               
                     </tr>
                 </thead>
                 <tbody>
+                
                     {users.map((user) => (
                         <tr key={user._id}>
                             <Link to={`/TissueBoxd/profile/${user._id}`}>
                                 <td>{user.username}</td>
-                                <td>{user.firstName}</td>
-                                <td>{user.lastName}</td>
+                    
                             </Link>
+                            <td>{user.role}</td>
                             <td>
                                 <button onClick={() => deleteUser(user)}>
                                     <BsTrash3Fill />
@@ -121,8 +194,9 @@ function UserTable() {
                         </tr>
                     ))}
                 </tbody>
-            </table>
-        </div>
+                    </table>
+                </div>
+            </div>
     );
 }
 export default UserTable;
