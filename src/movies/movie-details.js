6235264223069b8d360cbcf60f6696f182d1ad09
Adrian Tam <tam.ad@northeastern.edu>
2023-12-13 05:26:40 -0500

    movie details page

--- src/movies/movie-details.js
+++ src/movies/movie-details.js
@@ -1,92 +1,163 @@
-import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import * as client from './movie-service';
-import * as likesClient from '../likes/client';
-import * as userService from '../users/client';
-import { useState } from 'react';
-import ProtectedContent from '../users/protectedContent';
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import * as client from "./movie-service";
+import * as likesClient from "../likes/client";
+import * as userService from "../users/client";
+import { useState } from "react";
+import ProtectedContent from "../users/protectedContent";
+import "./movie-details.css";
+
 function MovieDetails() {
-    const [movie, setMovie] = useState(null);
-    const [currentUser, setCurrentUser] = useState(null);
-    const [clicked, setClicked] = useState(false);
-    const [likes, setLikes] = useState([]);
-    const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [clicked, setClicked] = useState(false);
+  const [likes, setLikes] = useState([]);
+  const { id } = useParams();
 
-    const fetchMovie = async (id) => {
-        const result = await client.imdbIDSearch(id);
-        setMovie(result.data);
-        fetchLikes();
-    };
+  const fetchMovie = async (id) => {
+    const result = await client.imdbIDSearch(id);
+    setMovie(result.data);
+    fetchLikes();
+  };
 
-    const like = async () => {
-        if (movie) {
-            await likesClient.createUserLikesMovie(
-                currentUser._id,
-                currentUser.firstName + ' ' + currentUser.lastName,
-                id,
-                movie.Title,
-                {poster: movie.Poster},
-            );
-            setClicked(true);
-        }
-    };
+  const like = async () => {
+    if (movie) {
+      await likesClient.createUserLikesMovie(
+        currentUser._id,
+        currentUser.firstName + " " + currentUser.lastName,
+        id,
+        movie.Title,
+        { poster: movie.Poster }
+      );
+      setClicked(true);
+    }
+  };
 
-    const unlike = async () => {
-        if (movie) {
-            await likesClient.deleteUserLikesMovie(currentUser._id, id);
-            setClicked(true);
-        }
-    };
+  const unlike = async () => {
+    if (movie) {
+      await likesClient.deleteUserLikesMovie(currentUser._id, id);
+      setClicked(true);
+    }
+  };
 
-    const fetchLikes = async () => {
-        const likes = await likesClient.findUsersWhoLikeMovie(id);
-        setLikes(likes);
-    };
+  const fetchLikes = async () => {
+    const likes = await likesClient.findUsersWhoLikeMovie(id);
+    setLikes(likes);
+  };
 
-    const fetchCurrentUser = async () => {
-        const user = await userService.account();
-        setCurrentUser(user);
-    };
+  const fetchCurrentUser = async () => {
+    const user = await userService.account();
+    setCurrentUser(user);
+  };
 
-    const alreadyLiked = () => {
-        return likes.find((likes) => likes.user === currentUser._id);
-    };
+  const alreadyLiked = () => {
+    return likes.find((likes) => likes.user === currentUser._id);
+  };
 
-    useEffect(() => {
-        fetchMovie(id);
-        fetchCurrentUser();
-    }, [id]);
+  const handleRefreshLike = () => {
+    like();
+    window.location.reload();
+  };
 
-    return (
-        <div>
-            <ProtectedContent>
-                {!alreadyLiked() ? (
-                    <button onClick={like} disabled={clicked} className="btn btn-primary float-end">
-                        Like
-                    </button>
-                ) : (
-                    <div>
-                        <button onClick={unlike} disabled={clicked} className="btn btn-danger float-end">
-                            Unlike
-                        </button>
-                    </div>
-                )}
-            </ProtectedContent>
-            <h1>{movie && movie.Title}</h1>
-            <div>{movie && <>{JSON.stringify(movie)}</>}</div>
-            Likes:
-            <div>{likes.length}</div>
-            <div>
-                {likes.map((user) => (
-                    <div>
-                        <Link key={user.user} to={`/TissueBoxd/profile/${user.user}`}>
-                            {user.userName}
-                        </Link>
-                    </div>
-                ))}
-            </div>
+  const handleRefreshUnlike = () => {
+    unlike();
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    fetchMovie(id);
+    fetchCurrentUser();
+  }, [id]);
+
+  return (
+    <div>
+      <h1 className="movie-title">{movie && movie.Title}</h1>
+
+      <div className="row">
+        <div className="col-md-4 bg-light p-4 rounded">
+          <p className="mb-3">
+            {movie && movie.Year} {"\u00B7"} {movie && movie.Rated} {"\u00B7"}{" "}
+            {movie && movie.Runtime}
+          </p>
+          <img
+            src={movie && movie.Poster}
+            alt="The Movie Poster"
+            className="img-fluid mb-3"
+          />
+          <p className="mb-1">
+            <strong>Director(s):</strong> {movie && movie.Director}
+          </p>
+          <p className="mb-1">
+            <strong>Writer(s):</strong> {movie && movie.Writer}
+          </p>
+          <p className="mb-1">
+            <strong>Actors:</strong> {movie && movie.Actors}
+          </p>
+          <a
+            href={movie ? `https://www.imdb.com/title/${movie.imdbID}` : "#"}
+            className="btn btn-warning me-2"
+          >
+            Go to IMDb
+          </a>
+          <ProtectedContent>
+            {!alreadyLiked() ? (
+              <button
+                onClick={handleRefreshLike}
+                disabled={clicked}
+                className="btn btn-success ms-2"
+              >
+                Like
+              </button>
+            ) : (
+              <button
+                onClick={handleRefreshUnlike}
+                disabled={clicked}
+                className="btn btn-danger ms-2"
+              >
+                Unlike
+              </button>
+            )}
+          </ProtectedContent>
         </div>
-    );
+
+        <div className="col-md-8 bg-white p-4 rounded shadow description">
+          <p className="mb-3">
+            <strong>Released:</strong> {movie && movie.Released}
+          </p>
+          <p className="mb-3">
+            <strong>Genre:</strong> {movie && movie.Genre}
+          </p>
+          <p className="mb-3">
+            <strong>Plot:</strong>
+            <br />
+            {movie && movie.Plot}
+          </p>
+          <p className="mb-3">
+            <strong>Language:</strong> {movie && movie.Language}
+          </p>
+          <p className="mb-3">
+            <strong>Country:</strong> {movie && movie.Country}
+          </p>
+          <p className="mb-3">
+            <strong>Awards:</strong> {movie && movie.Awards}
+          </p>
+        </div>
+      </div>
+
+      <hr></hr>
+
+      <h3>Likes: {likes.length}</h3>
+      <div>
+        {likes.map((user) => (
+          <div>
+            <Link key={user.user} to={`/TissueBoxd/profile/${user.user}`}>
+              {user.userName}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default MovieDetails;
--- src/movies/movie-search.js
+++ src/movies/movie-search.js
@@ -1,26 +1,41 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 function MovieSearch() {
-    const [searchText, setSearchText] = useState('Avengers Endgame');
-    const navigate = useNavigate();
+  const [searchText, setSearchText] = useState("Pokemon");
+  const navigate = useNavigate();
 
+  const search = async (text) => {
+    navigate(`/TissueBoxd/search/${text}`);
+  };
 
-    const search = async (text) => {
-        navigate(`/TissueBoxd/search/${text}`);
-    };
-
-    return (
-        <div class = "center-container-movie">
-            <input value={searchText} className="form-control w-100" onChange={(e) => setSearchText(e.target.value)} />
-            <button className="btn btn-primary search-button" onClick={() => search(searchText)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 20 20">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-</svg> Search By Title 
-            </button>
-        </div>
-    );
+  return (
+    <div class="center-container-movie">
+      <input
+        value={searchText}
+        className="form-control w-100"
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Enter a movie title" 
+      />
+      <button
+        className="btn btn-info search-button"
+        onClick={() => search(searchText)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 20 20"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+        </svg>{" "}
+        Search By Title
+      </button>
+    </div>
+  );
 }
 
 export default MovieSearch;
